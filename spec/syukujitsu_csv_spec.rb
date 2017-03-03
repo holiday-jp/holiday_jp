abe4920511b9a8770d108d32554a4da173f62a89
k1LoW <k1lowxb@gmail.com>
2017-03-03 18:57:19 +0900

    Add spec (3条2項, 3条3項)

--- spec/syukujitsu_csv_spec.rb
+++ spec/syukujitsu_csv_spec.rb
@@ -13,11 +13,28 @@ context 'Check holidays.yml by syukujitsu.csv' do
     @cholidays = CSV.parse(csv, headers: true, encoding: 'Shift_JIS')
   end
 
-  it 'holidays.yml shoud have date of syukujitsu.csv' do
+  it '3条1項 「国民の祝日」は、休日とする。' do
     @cholidays.each do |row|
       d = Date::parse(row[0])
       expect(@holidays.key?(d)).to eq true
       expect(@holidays[d]).to eq row[1].encode('UTF-8', 'Shift_JIS')
+    end
+  end
+
+  it '3条2項 「国民の祝日」が日曜日に当たるときは、その日後においてその日に最も近い「国民の祝日」でない日を休日とする。' do
+    @cholidays.each do |row|
+      d = Date::parse(row[0])
+      expect(@holidays.key?(d + 1)).to eq true if d.sunday?
+    end
+  end
+
+  it '3条3項 その前日及び翌日が「国民の祝日」である日（「国民の祝日」でない日に限る。）は、休日とする。' do
+    @cholidays.each do |row|
+      d = Date::parse(row[0])
+      key = (d + 2).strftime('%Y-%m-%d')
+      expect(@holidays.key?(d + 1)).to eq true if @cholidays.find do |r|
+        r[0] == key
+      end
     end
   end
 end
